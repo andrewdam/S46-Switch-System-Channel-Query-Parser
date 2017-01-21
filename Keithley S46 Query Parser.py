0ce31ba91c81ac45efae26b592026b5a66445218
Andrew Dam <adam062@uottawa.ca>
2017-01-21 18:20:38 -0500

    Initial commit.

---
+++ Keithley S46 Query Parser.py
@@ -0,0 +1,36 @@
+test_in = [
+    '[@1,8,15,24]',
+    '[@24]',
+    '[@16,19]',
+    '[@]',
+    '[@1]',
+    '[@15]'
+    ]
+
+def f(str_in):
+    
+    lst_in = map(int, str_in[2:-1].split(',')) if len(str_in) > 3 else []
+    
+    result = [0] * 4
+    for x in lst_in:
+        result[(x-1)/6] = x
+    return result
+
+for t in test_in:
+    res = f(t)
+    print 'Original: {}'.format(t)
+    print 'Result: {}, {}, {}, {}'.format(res[0], res[1], res[2], res[3])
+
+
+##import re
+##
+##so  basically use '\d\d|\d' in a list now
+##
+##result =[0,0,0,0]
+##for x in regexList:
+##    if x in xrange(1,7):
+##        result[0] = x
+##        
+##    elif x in xrange(1,7):
+##        result[0] = x
+        
